46be20bd17fb504808a936033aea822e0b981f5c
arafathnihar <arafathnihar@gmail.com>
2015-10-03 14:10:05 +0530

    TabbPane added

--- src/Controller/DashboardController.java
+++ src/Controller/DashboardController.java
@@ -4,26 +4,32 @@ import java.net.*;
 import java.util.*;
 import javafx.fxml.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.*;
 
 public class DashboardController implements Initializable {
 
+    @FXML
+    private AnchorPane invoiceTab;
+    @FXML
+    private AnchorPane billTab;
+    @FXML
+    private AnchorPane productTab;
+    @FXML
+    private AnchorPane distributerTab;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
-    }
-
-    public void testing() {
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("/resource/Invoice.fxml"));
-            Stage stage = new Stage();
-            stage.setTitle("");
-            stage.setScene(new Scene(root, 915, 580));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
+       try {
+           invoiceTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Invoice.fxml")));
+           billTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Bill.fxml"))); 
+           productTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Product.fxml")));
+           distributerTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Distributer.fxml")));
+       } catch (IOException ex) {
+           
         }
     }
 }
