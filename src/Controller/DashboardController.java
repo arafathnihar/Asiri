7cc83cb44daf35a04f7533011a301dbf0a059ed6
arafathnihar <arafathnihar@gmail.com>
2015-11-08 13:50:36 +0530

    tab refresh on click added

--- src/Controller/DashboardController.java
+++ src/Controller/DashboardController.java
@@ -1,35 +1,137 @@
 package Controller;
 
+import Model.NotificationModel;
 import java.net.*;
 import java.util.*;
 import javafx.fxml.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.Event;
 import javafx.scene.*;
+import javafx.scene.control.Tab;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 public class DashboardController implements Initializable {
 
     @FXML
-    private VBox invoiceTab;
+    private VBox invoiceTabVbx;
     @FXML
-    private VBox billTab;
+    private VBox billTabVbx;
     @FXML
-    private VBox productTab;
+    private VBox productTabVbx;
     @FXML
-    private VBox distributerTab;
+    private VBox distributerTabVbx;
     @FXML
-    private VBox notificationTab;
-    
+    private VBox notificationTabVbx;
+    @FXML
+    private ImageView notifyingImage;
+    @FXML
+    private Tab invoiceTab;
+    @FXML
+    private Tab billTab;
+    @FXML
+    private Tab productTab;
+    @FXML
+    private Tab distributerTab;
+    @FXML
+    private Tab notificationTab;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        invoiceTab.setOnSelectionChanged((Event t) -> {
+            if (invoiceTab.isSelected()) {
+                reloadInvoiceTab();
+            }
+        });
+        billTab.setOnSelectionChanged((Event t) -> {
+            if (billTab.isSelected()) {
+                reloadBillTab();
+            }
+        });
+        productTab.setOnSelectionChanged((Event t) -> {
+            if (productTab.isSelected()) {
+                reloadProductTab();
+            }
+        });
+        distributerTab.setOnSelectionChanged((Event t) -> {
+            if (distributerTab.isSelected()) {
+                reloadDistributerTab();
+            }
+        });
+        notificationTab.setOnSelectionChanged((Event t) -> {
+            if (notificationTab.isSelected()) {
+                reloadNotificationTab();
+            }
+        });
         try {
-            invoiceTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Invoice.fxml")));
-            billTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Bill.fxml")));
-            productTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Product.fxml")));
-            distributerTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Distributer.fxml")));
-            notificationTab.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Notification.fxml")));
+            invoiceTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Invoice.fxml")));
+            billTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Bill.fxml")));
+            productTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Product.fxml")));
+            distributerTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Distributer.fxml")));
+            NotificationModel nm = new NotificationModel();
+            if (nm.minStockNotification() != null && !nm.minStockNotification().isEmpty()) {
+                Image i = new Image("/resource/images/notificationNotChecked.png");
+                notifyingImage.setImage(i);
+            }
+            notificationTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Notification.fxml")));
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public void reloadInvoiceTab() {
+        invoiceTabVbx.getChildren().clear();
+        try {
+            invoiceTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Invoice.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void reloadBillTab() {
+        billTabVbx.getChildren().clear();
+        try {
+            billTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Bill.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void reloadProductTab() {
+        productTabVbx.getChildren().clear();
+        try {
+            productTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Product.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void reloadDistributerTab() {
+        distributerTabVbx.getChildren().clear();
+        try {
+            distributerTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Distributer.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void reloadNotificationTab() {
+        NotificationModel nm = new NotificationModel();
+        if (nm.minStockNotification() != null && !nm.minStockNotification().isEmpty()) {
+            Image i = new Image("/resource/images/notificationNotChecked.png");
+            notifyingImage.setImage(i);
+        } else {
+            Image i = new Image("/resource/images/notificationChecked.png");
+            notifyingImage.setImage(i);
+        }
+        notificationTabVbx.getChildren().clear();
+        try {
+            notificationTabVbx.getChildren().setAll((Node) FXMLLoader.load(getClass().getResource("/resource/Notification.fxml")));
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
--- src/Controller/NotificationController.java
+++ src/Controller/NotificationController.java
@@ -5,10 +5,15 @@ package Controller;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import Model.NotificationModel;
+import Model.Product;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -17,12 +22,36 @@ import javafx.fxml.Initializable;
  */
 public class NotificationController implements Initializable {
 
+    @FXML
+    private TableView< Product> outOfStockTable;
+    @FXML
+    private TableColumn< Product, String> productIDC;
+    @FXML
+    private TableColumn< Product, String> productNameC;
+    @FXML
+    private TableColumn< Product, String> currentStockC;
+    @FXML
+    private TableColumn< Product, String> minStockC;
+   
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        productIDC.setCellValueFactory(new PropertyValueFactory<>("productID"));
+        productNameC.setCellValueFactory(new PropertyValueFactory<>("productName"));
+        currentStockC.setCellValueFactory(new PropertyValueFactory<>("productStock"));
+        minStockC.setCellValueFactory(new PropertyValueFactory<>("productMinStock"));
+        NotificationModel nm = new NotificationModel();
+        outOfStockTable.setItems(nm.minStockNotification());
     }    
     
+    public TableView<Product> getOutOfStockTable() {
+        return outOfStockTable;
+    }
+
+    public void setOutOfStockTable(TableView<Product> outOfStockTable) {
+        this.outOfStockTable = outOfStockTable;
+    }
 }
