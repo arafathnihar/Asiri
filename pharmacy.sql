e454d7a273a5da6153036a0723e6784e4911b271
arafathnihar <arafathnihar@gmail.com>
2015-12-28 15:05:31 +0530

    product stock field removed

--- pharmacy.sql
+++ pharmacy.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2015 at 05:40 AM
+-- Generation Time: Dec 28, 2015 at 10:33 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
