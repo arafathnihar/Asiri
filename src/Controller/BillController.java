b375c72cc726896ae21c89b38268acbc646ef2aa
dilushi <dilu9218@gmail.com>
2016-01-01 21:08:08 +0530

    billController validation and BillID added to UI

--- src/Controller/BillController.java
+++ src/Controller/BillController.java
@@ -19,10 +19,16 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import org.controlsfx.control.textfield.TextFields;
 
 public class BillController implements Initializable {
 
+    @FXML
+    private TextField billID;
     @FXML
     private TextField billAmount;
     @FXML
@@ -52,6 +58,8 @@ public class BillController implements Initializable {
     @FXML
     private TableColumn< BillItem, Double> totalC;
     @FXML
+    private Label BillIDLbl;
+    @FXML
     private Label LabeldateLbl;
     @FXML
     private Label discountLbl;
@@ -67,6 +75,13 @@ public class BillController implements Initializable {
     private Label quantityLbl;
     @FXML
     private Label priceLbl;
+    @FXML
+    private Label messageLabel;
+    
+    //Image imageDistri = new Image(getClass().getResourceAsStream("/resource/images/distributer.png"));
+    Image imageError = new Image(getClass().getResourceAsStream("/resource/images/error.png"));
+    Image imageSuccess = new Image(getClass().getResourceAsStream("/resource/images/success.png"));
+    Image imageWarnning = new Image(getClass().getResourceAsStream("/resource/images/warnning.png"));
 
     private BillItemModel bm = new BillItemModel();
     private ProductModel pm = new ProductModel();
@@ -111,7 +126,16 @@ public class BillController implements Initializable {
             total.setText(Double.toString(tot));
         }
     }
-
+     public boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        } catch (NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
     public static boolean isNumeric(String str) {
         try {
             double d = Double.parseDouble(str);
@@ -154,65 +178,169 @@ public class BillController implements Initializable {
             productID.getSelectionModel().clearSelection();
         }
     }
+    
+    public boolean isValidBill() {
+        if (billDate.getValue()==null) {
+            LabeldateLbl.setText("Required!");
+            return false;
+        } else if (billID.getText().isEmpty()) {
+            BillIDLbl.setText("Required!");
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public boolean isValidBillItem() {
+        if (productID.getSelectionModel().isEmpty()) {
+            productIDLbl.setText("Required!");
+            return false;
+        } else if (quantity.getText().isEmpty()) {
+            quantityLbl.setText("Required!");
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+    public void add() {
+                    if (isValidBillItem()){
+                            if (isInteger(quantity.getText())) {
+
+                                        BillItem bi = new BillItem();
+                                        bi.setProductID(productID.getValue());
+                                        bi.setUnitPrice(Double.parseDouble(unitPrice.getText()));
+                                        bi.setQuantity(Integer.parseInt(quantity.getText()));
+                                        bi.setTotal(Double.parseDouble(total.getText()));
+                                        itemsTable.getItems().add(bi);
+                                        total();
+                                        clear();
+                                        //icon.setImage(imageSuccess);
+                                        messageLabel.setTextFill(Color.GREEN);
+                                        messageLabel.setText("New Bill item added");
+
+                                    } else {
+                                                //icon.setImage(imageError);
+                                        quantityLbl.setText("It's not a number");
+                                        messageLabel.setTextFill(Color.RED);
+                                        messageLabel.setText(" Quantity should be a numeric value ");
+                                    }
+                            } 
+	else{
+			//icon.setImage(imageError);
+			messageLabel.setTextFill(Color.RED);
+			messageLabel.setText(" Fill all fields ");
+		}
+    }
+
+        /*
     @FXML
     public void add() {
-        BillItem bi = new BillItem();
-        bi.setProductID(productID.getValue());
-        bi.setUnitPrice(Double.parseDouble(unitPrice.getText()));
-        bi.setQuantity(Integer.parseInt(quantity.getText()));
-        bi.setTotal(Double.parseDouble(total.getText()));
-        itemsTable.getItems().add(bi);
-        total();
-        clear();
-    }
+        if (isValidBillItem()) {
+                BillItem bi = new BillItem();
+                bi.setProductID(productID.getValue());
+                bi.setUnitPrice(Double.parseDouble(unitPrice.getText()));
+                bi.setQuantity(Integer.parseInt(quantity.getText()));
+                bi.setTotal(Double.parseDouble(total.getText()));
+                itemsTable.getItems().add(bi);
+                total();
+                clear();
+                //icon.setImage(imageSuccess);
+                messageLabel.setTextFill(Color.GREEN);
+                messageLabel.setText("New Bill item added");
+        } else {
+            //icon.setImage(imageError);
+            messageLabel.setTextFill(Color.RED);
+            messageLabel.setText(" Fill all fields ");
+        }
+    }*/
 
     @FXML
     public void save() {
-        b.setBillNote(billNote.getText());
-        b.setBillAmount(Double.parseDouble(billAmount.getText()));
-        ObservableList< BillItem> items = itemsTable.getItems();
-        int j = 1;
-        for (int i = 0; i < items.size(); i++) {
-            items.get(i).setBillItemNo("Item-" + j);
-            j++;
+        if (isValidBill()){
+            b.setBillNote(billNote.getText());
+            b.setBillAmount(Double.parseDouble(billAmount.getText()));
+            ObservableList< BillItem> items = itemsTable.getItems();
+            int j = 1;
+            for (int i = 0; i < items.size(); i++) {
+                items.get(i).setBillItemNo("Item-" + j);
+                j++;
+            }
+            bm.addBill(b, items);
+            clear();
+            for (int i = 0; i < itemsTable.getItems().size(); i++) {
+                itemsTable.getItems().clear();
+            }
         }
-        bm.addBill(b, items);
-        clear();
-        for (int i = 0; i < itemsTable.getItems().size(); i++) {
-            itemsTable.getItems().clear();
+        else{
+            //icon.setImage(imageError);
+            messageLabel.setTextFill(Color.RED);
+            messageLabel.setText(" Fill all fields ");
         }
     }
 
     @FXML
     public void edit() {
         index = itemsTable.getSelectionModel().getSelectedIndex();
-        BillItem bi = itemsTable.getItems().get(index);
-        productID.setValue(bi.getProductID());
-        unitPrice.setText(String.valueOf(bi.getUnitPrice()));
-        quantity.setText(String.valueOf(bi.getQuantity()));
-        total.setText(String.valueOf(bi.getTotal()));
+        if (index >= 0) {
+            BillItem bi = itemsTable.getItems().get(index);
+            productID.setValue(bi.getProductID());
+            unitPrice.setText(String.valueOf(bi.getUnitPrice()));
+            quantity.setText(String.valueOf(bi.getQuantity()));
+            total.setText(String.valueOf(bi.getTotal()));
+        } else {
+           // icon.setImage(imageWarnning);
+            messageLabel.setTextFill(Color.ORANGE);
+            messageLabel.setText("Please select a distributer to edit");
+        }
     }
 
     @FXML
     public void update() {
-        BillItem bi = new BillItem();
-        bi.setProductID(productID.getValue());
-        bi.setUnitPrice(Double.parseDouble(unitPrice.getText()));
-        bi.setQuantity(Integer.parseInt(quantity.getText()));
-        bi.setTotal(Double.parseDouble(total.getText()));
-        index = itemsTable.getSelectionModel().getSelectedIndex();
-        itemsTable.getItems().set(index, bi);
-        total();
-        clear();
+        if (isValidBillItem()){
+                if (isInteger(quantity.getText())) {
+                        BillItem bi = new BillItem();
+                        bi.setProductID(productID.getValue());
+                        bi.setUnitPrice(Double.parseDouble(unitPrice.getText()));
+                        bi.setQuantity(Integer.parseInt(quantity.getText()));
+                        bi.setTotal(Double.parseDouble(total.getText()));
+                        index = itemsTable.getSelectionModel().getSelectedIndex();
+                        itemsTable.getItems().set(index, bi);
+                        total();
+                        clear();
+                }
+                 else {
+                                                //icon.setImage(imageError);
+                            quantityLbl.setText("It's not a number");
+                            messageLabel.setTextFill(Color.RED);
+                            messageLabel.setText(" Quantity should be a numeric value ");
+                }
+        }
+                             
+	else{
+                //icon.setImage(imageError);
+                messageLabel.setTextFill(Color.RED);
+                messageLabel.setText(" Fill all fields ");
+	}
+                        
     }
 
     @FXML
     public void delete() {
         index = itemsTable.getSelectionModel().getSelectedIndex();
-        itemsTable.getItems().remove(index);
-        total();
-        clear();
+        if (index >= 0) {
+            itemsTable.getItems().remove(index);
+            total();
+            clear();
+           // icon.setImage(imageSuccess);
+            messageLabel.setTextFill(Color.GREEN);
+            messageLabel.setText("Bill Item is deleted");
+            clear();
+        } else {
+            //icon.setImage(imageWarnning);
+            messageLabel.setTextFill(Color.ORANGE);
+            messageLabel.setText(" Please select a BillItem to Delete ");
+        }
     }
 
     @FXML
@@ -223,11 +351,56 @@ public class BillController implements Initializable {
         quantity.clear();
         total.clear();
         billNote.clear();
+        
+        productIDLbl.setText("");
+        quantityLbl.setText("");
+        LabeldateLbl.setText("");
+        
+        /*codeSearch.clear();
+        nameSearch.clear();
+        addressSearch.clear();
+        phoneNoSearch.clear(); */
     }
 
     @FXML
     public void cancel() {
 
+    }
+    
+    @FXML
+    public void clearAll() {
+        clear();
+        onPressAnything();
+    }
+    
+    @FXML
+    public void billIDonPress() {
+       BillIDLbl.setText("");
+        onPressAnything();
+    }
+    
+    @FXML
+    public void dateonPress() {
+        LabeldateLbl.setText("");
+        onPressAnything();
+    }
+    
+    @FXML
+    public void ProductIDonPress() {
+        productIDLbl.setText("");
+        onPressAnything();
+    }
+    
+    @FXML
+    public void quantityonPress() {
+        quantityLbl.setText("");
+        onPressAnything();
+    }
+    
+    @FXML
+    public void onPressAnything() {
+        messageLabel.setText("");
+        //icon.setImage(imageDistri);
     }
 
 }
